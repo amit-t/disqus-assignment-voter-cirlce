52e464e8f6b8747ead9c0c6af3b819f7341e3312
Amit Tiwari <amit.t@xoxoday.com>
2019-10-21 17:19:15 +0530

    Comments Section UI Complted, Input Comment Box UI Pending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,28 @@ export class AppComponent {
       'time': 'Today at 5:43 PM',
       'comment': 'This is Aweomse!',
 
+    },
+    {
+      'name': 'Elliot Fu',
+      'src': 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=wavatar&f=y',
+      'time': 'Yesterday at 5:43 PM',
+      'comment': 'This has been helpful!',
+      'comments': [
+        {
+          'name': 'Matt',
+          'src': 'https://s3.amazonaws.com/uifaces/faces/twitter/dancounsell/73.jpg',
+          'time': 'Just now',
+          'comment': 'You are right!',
+        }
+      ]
+
+    },
+    {
+      'name': 'Joe Henderson',
+      'src': 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=retro&f=y',
+      'time': '5 days ago',
+      'comment': 'Thankyou dude!',
+
     }
   ];
 }
